c57ccaaaee6b2c96221f78423351254313313b3d
yuanchao <yuanchao@feicuiedu.com>
2016-07-14 16:00:50 +0800

    Home模块,主页面搭建

--- app/src/main/java/com/feicuiedu/treasure/home/HomeActivity.java
+++ app/src/main/java/com/feicuiedu/treasure/home/HomeActivity.java
@@ -1,15 +1,70 @@
 package com.feicuiedu.treasure.home;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.feicuiedu.treasure.R;
+import com.feicuiedu.treasure.commons.ActivityUtils;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @Bind(R.id.nav_view) NavigationView navigationView;
+
+    private ActivityUtils activityUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activityUtils = new ActivityUtils(this);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override public void onContentChanged() {
+        super.onContentChanged();
+        ButterKnife.bind(this);
+        //
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);// 关闭title
+        navigationView.setNavigationItemSelectedListener(this); // 监听
+        //
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_hide: // 埋藏宝藏
+                activityUtils.showToast(R.string.hide_treasure);
+                break;
+        }
+        // 返回true,当前选项变为checked状态
+        return false;
+    }
+
+    @Override public void onBackPressed() {
+        // 是不是打开的(左)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return;
+        }
+        super.onBackPressed();
     }
 }
