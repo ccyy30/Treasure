bcaabbdd70ee3778c427e9da56c9cff9cfdadd9f
yuanchao <yuanchao@feicuiedu.com>
2016-07-18 14:47:50 +0800

    BaiduMap,地图基本操作

--- app/src/main/java/com/feicuiedu/treasure/home/map/MapFragment.java
+++ app/src/main/java/com/feicuiedu/treasure/home/map/MapFragment.java
@@ -11,13 +11,17 @@ import android.widget.FrameLayout;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.feicuiedu.treasure.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
+ * 1. MapView基本操作
+ * 2. 定位图层(当前我的位置)
  * 作者：yuanchao on 2016/7/18 0018 14:05
  * 邮箱：yuanchao@feicuiedu.com
  */
@@ -34,7 +38,7 @@ public class MapFragment extends Fragment {
         initBaiduMap();
     }
 
-    @Bind(R.id.map_frame)FrameLayout mapFrame;
+    @Bind(R.id.map_frame) FrameLayout mapFrame;
     private MapView mapView; // 地图视图
     private BaiduMap baiduMap; // 地图视图操作类
 
@@ -53,6 +57,35 @@ public class MapFragment extends Fragment {
         // 拿到当前MapView的控制器
         baiduMap = mapView.getMap();
         // 在当前Layout上添加MapView
-        mapFrame.addView(mapView,0);
+        mapFrame.addView(mapView, 0);
     }
+
+    // 地图缩放操作
+    @OnClick({R.id.iv_scaleDown,R.id.iv_scaleUp})
+    public void scaleMap(View view){
+        switch (view.getId()){
+            case R.id.iv_scaleUp:
+                baiduMap.setMapStatus(MapStatusUpdateFactory.zoomIn());
+                break;
+            case R.id.iv_scaleDown:
+                baiduMap.setMapStatus(MapStatusUpdateFactory.zoomOut());
+                break;
+        }
+    }
+
+    // 地图类别更新
+    @OnClick(R.id.tv_satellite)
+    public void switchMapType(){
+        int type = baiduMap.getMapType();
+        type = type == BaiduMap.MAP_TYPE_NORMAL ? BaiduMap.MAP_TYPE_SATELLITE : BaiduMap.MAP_TYPE_NORMAL;
+        baiduMap.setMapType(type);
+    }
+
+    // 指南针更新
+    @OnClick(R.id.tv_compass)
+    public void switchCompass(){
+        boolean isCompass = baiduMap.getUiSettings().isCompassEnabled();
+        baiduMap.getUiSettings().setCompassEnabled(!isCompass);
+    }
+
 }
