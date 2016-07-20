211317953084320e1ebd4777334734aa796012b1
yuanchao <yuanchao@feicuiedu.com>
2016-07-20 17:21:44 +0800

    HideTreasure,结构mvp

--- app/src/main/java/com/feicuiedu/treasure/treasure/home/hide/HideTreasureActivity.java
+++ app/src/main/java/com/feicuiedu/treasure/treasure/home/hide/HideTreasureActivity.java
@@ -3,24 +3,29 @@ package com.feicuiedu.treasure.treasure.home.hide;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.baidu.mapapi.model.LatLng;
 import com.feicuiedu.treasure.R;
 import com.feicuiedu.treasure.commons.ActivityUtils;
+import com.hannesdorfmann.mosby.mvp.MvpActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class HideTreasureActivity extends AppCompatActivity {
+public class HideTreasureActivity extends MvpActivity<HideTreasureView,HideTreasurePresenter> implements HideTreasureView {
 
     private static final String EXTRA_KEY_TITLE = "key_title";
     private static final String EXTRA_KEY_LOCATION = "key_location";
     private static final String EXTRA_KEY_LAT_LNG = "key_latlng";
     private static final String EXTRA_KEY_ALTITUDE = "key_altitude";
 
-    /** 进入当前Activity*/
+    /**
+     * 进入当前Activity
+     */
     public static void open(Context context, String title, String location, LatLng latLng, double altitude) {
         Intent intent = new Intent(context, HideTreasureActivity.class);
         intent.putExtra(EXTRA_KEY_TITLE, title);
@@ -31,7 +36,7 @@ public class HideTreasureActivity extends AppCompatActivity {
     }
 
     private ActivityUtils activityUtils;
-    @Bind(R.id.toolbar)Toolbar toolbar;
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +53,45 @@ public class HideTreasureActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle(getIntent().getStringExtra(EXTRA_KEY_TITLE));
         }
+    }
+
+    @NonNull @Override public HideTreasurePresenter createPresenter() {
+        return new HideTreasurePresenter();
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_hide_treasure, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            // 确定上传
+            case R.id.action_send:
+                // 执行业务
+                // Presenter做业务
+
+                break;
+        }
+        return true;
+    }
+
+    @Override public void showProgress() {
+
+    }
+
+    @Override public void hideProgress() {
+
+    }
+
+    @Override public void showMessage(String msg) {
+
+    }
+
+    @Override public void navigateToHome() {
+
     }
 }
---
+++ app/src/main/java/com/feicuiedu/treasure/treasure/home/hide/HideTreasurePresenter.java
@@ -0,0 +1,17 @@
+package com.feicuiedu.treasure.treasure.home.hide;
+
+import com.hannesdorfmann.mosby.mvp.MvpNullObjectBasePresenter;
+
+/**
+ * 作者：yuanchao on 2016/7/20 0020 17:19
+ * 邮箱：yuanchao@feicuiedu.com
+ */
+public class HideTreasurePresenter extends MvpNullObjectBasePresenter<HideTreasureView>{
+
+    /**
+     * 本类核心业务
+     */
+    public void hideTreasure() {
+
+    }
+}
